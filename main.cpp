d7f86d70e22f03da669d5adffb21e41233af53c6
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2022-07-18 10:20:42 +0200

    Add files via upload
    Files reuploaded on 18.07.2022.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"This program counts from 1 to 10:"<<endl;
+
+    for(int i=1; i<=10; i++)
+    {
+        cout << i << endl;
+    }
+
+   cout<<"Press any key to exit";
+   getchar();
+   return 0;
+}
